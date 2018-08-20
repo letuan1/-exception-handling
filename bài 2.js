0043fa9858a6c2b363293d3eac998ac2890ec491
letuan1 <41882914+letuan1@users.noreply.github.com>
2018-08-20 16:11:05 +0700

    Update bài 2.js

--- bài 2.js
+++ bài 2.js
@@ -1,10 +1,10 @@
 function main() {
     var x = prompt("nhập tên để kiểm tra");
     try {
-        if (isNaN(x)) throw "số";
-        if (x == 'a') throw "số"
-        console.log("tên hợp lệ")
+        if (isNaN(x) == false) throw "số"
+        if (x == true) throw "số"
+        console.log("tên đúng")
     }catch (e) {
-        console.log("tên không hợp lệ")
+        console.log("tên không hợp lệ   " + e.message)
     }
 }
